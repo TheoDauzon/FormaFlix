71698b37aec9d99d2542c13fac5cba3cf44d2ee8
melissafsn <melifois@hotmail.fr>
2021-09-22 16:27:49 +0200

    test 22/09

---
+++ controllers/Formation.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace controllers;
+
+use controllers\base\Web;
+use models\CompetenceModel;
+use models\FormationModel;
+use utils\SessionHelpers;
+
+/**
+ * Contrôleur des formations
+ * Affichage de la liste des formations.
+ */
+class Formation extends Web
+{
+    private $formationModel;
+
+    function __construct()
+    {
+        $this->formationModel = new FormationModel();
+    }
+
+
+    // Affichage de la page d'accueil avec en fonction si connecté ou non une liste plus complète.
+    function home()
+    {
+        $formations = [];
+        if (SessionHelpers::isLogin()) {
+            // Récupération des vidéos par le modèle
+            $formations = $this->formationModel->getVideos();
+        } else {
+            $formations = $this->formationModel->getPublicVideos();
+        }
+        $this->header();
+        include("./views/formation/filter.php");
+        include("./views/formation/list.php");
+        $this->footer();
+    }
+
+    /**
+     * $id est automatiquement rempli via la valeur du GET
+     * @param $id
+     */
+    function tv($id)
+    {
+        // Récupération de la vidéo par rapport à l'ID demandé
+        $video = $this->formationModel->getByVideoId($id);
+        if (!$video) {
+            $this->redirect("./formations");
+        }
+
+        // Les vidéos non public ne doivent pas être visible si non connecté
+        if($video['VISIBILITEPUBLIC'] == 0 && !SessionHelpers::isLogin()){
+            $this->redirect("./formations");
+        }
+
+        // Compétences associées à la vidéo
+        $competences = $this->formationModel->competencesFormation($video["IDFORMATION"]);
+
+        $this->header();
+        include("./views/formation/tv.php");
+        $this->footer();
+    }
+}
---
+++ views/formation/filter.php
@@ -0,0 +1,25 @@
+<div class="container">
+    <div class="row pt-5">
+        <!-- <a class="dropdown-item disabled" href="#">Disabled link</a>-->
+        <div class="dropdown">
+            <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+                COMPETENCES
+            </a>
+
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+
+                <li><a class="dropdown-item" href="#">test</a></li>
+                <li><a class="dropdown-item" href="#">test</a></li>
+
+            </ul>
+        </div>
+        <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+            <option selected>Open this select menu</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+        </select>
+
+
+    </div>
+</div>
