eb97e02197aec80d98fe630c5f5d6c4bbe62a182
theod <theo.dauzon@gmail.com>
2021-09-29 21:44:12 +0200

    29/09

---
+++ configs.php
@@ -0,0 +1,15 @@
+<?php
+
+$DB_SERVER = getenv("MVC_SERVER") ?: "192.168.139.1";
+$DB_DATABASE = getenv("MVC_DB") ?: "la-triade";
+$DB_USER = getenv("MVC_USER") ?: "la-triade-2";
+$DB_PASSWORD = getenv("MVC_TOKEN") ?: "EZ2HqRIM";
+$DEBUG = getenv("MVC_DEBUG") ?: true;
+
+return array(
+    "DB_USER" => $DB_USER,
+    "DB_PASSWORD" => $DB_PASSWORD,
+    "DB_DSN" => "mysql:host=$DB_SERVER;dbname=$DB_DATABASE;charset=utf8",
+    "DEBUG" => $DEBUG
+);
+
