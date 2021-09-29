59bd009c599f15752453efd81ddbe1198ca340de
theod <theo.dauzon@gmail.com>
2021-09-29 21:44:12 +0200

    01/10b

---
+++ views/formation/filter.php
@@ -0,0 +1,16 @@
+<div class="container">
+    <div class="row pt-5">
+        <select name="filterCompet" class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+            <option selected>Selon quelle compétence?</option>
+            <?php
+            foreach ($competences as $competence) {
+            ?>
+            <option value=<?= $competence["IDCOMPETENCE"] ?>><?= $competence["LIBELLECOMPETENCE"] ?></option>
+
+                <?php
+            }
+
+            ?>
+        </select>
+    </div>
+</div>
