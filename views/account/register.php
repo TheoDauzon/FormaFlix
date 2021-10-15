5771a7711d9b832c7a915601dfd3bf5ee0268a95
tdauzon <theo.dauzon@gmail.com>
2021-10-15 14:41:29 +0200

    15/10/2021

--- views/account/register.php
+++ views/account/register.php
@@ -16,26 +16,26 @@
                                 <h1 class="h3 mb-3 fw-normal text-light">Inscription</h1>
 
                                 <div class="form-floating">
-                                    <input name="nom" type="text" class="form-control" id="floatingInput" placeholder="Nom">
+                                    <input name="nom" type="text" class="form-control" id="floatingInput" placeholder="Nom" required="required">
                                     <label for="floatingInput">Nom</label>
                                 </div>
 
                                 <div class="form-floating mt-2">
-                                    <input name="prenom" type="text" class="form-control" id="floatingInput" placeholder="Prénom">
+                                    <input name="prenom" type="text" class="form-control" id="floatingInput" placeholder="Prénom" required="required">
                                     <label for="floatingInput">Prénom</label>
                                 </div>
 
                                 <div class="form-floating mt-2">
-                                    <input name="mail" type="email" class="form-control" id="floatingInput" placeholder="Email">
+                                    <input name="mail" type="email" class="form-control" id="floatingInput" placeholder="Email" required="required">
                                     <label for="floatingInput">Adresse email</label>
                                 </div>
 
                                 <div class="form-floating mt-2">
-                                    <input name="mdp" type="password" class="form-control" id="floatingPassword" placeholder="Mot de passe">
+                                    <input name="mdp" type="password" class="form-control" id="floatingPassword" placeholder="Mot de passe" required="required">
                                     <label for="floatingPassword">Mot de passe</label>
                                 </div><br>
 
-                                <select name="filterDiplome" class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+                                <select name="filterDiplome" class="form-select form-select-lg mb-3" aria-label=".form-select-lg example" required="required">
                                     <option value="">Dernier diplôme obtenu</option>
                                     <?php
                                     foreach ($diplomes as $diplome) {
