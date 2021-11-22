639a64fdc43f5d8ac984bd0b540cf010ca882062
melissafsn <melifois@hotmail.fr>
2021-11-22 19:05:36 +0100

    22/11 table

--- views/account/gestionCommentaire.php
+++ views/account/gestionCommentaire.php
@@ -2,63 +2,37 @@
     <div class="row">
         <div class="card">
             <div class="card-body text-center">
-                <h1 class="text-center pb-3">Mes commentaires postés</h1>
-                <?php
-                    if (sizeof($commentaires) > 0) {
-                        ?>
-                <ul class="list-group pt-3">
-                    <li class="list-group-item">
-                        <div class="d-flex">
-                            <table>
-                                <tr>
-                                    <th>Image</th>
-                                    <th>Titre</th>
-                                    <th>Note</th>
-                                    <th>Commentaire</th>
-                                    <th>Date</th>
-                                    <th>Actions</th>
-                                </tr>
-                            </table>
-                        </div>
-                    </li>
+                <h1 class="text-center pb-3">Mes commentaires postés</h1> <br>
+                <?php if (sizeof($commentaires) > 0) { ?>
+                    <table  class="table table-hover" >
+                        <thead>
+                    <tr>
+                        <th scope="col" style="width:15%">Image</th>
+                        <th scope="col" style="width:25%">Titre</th>
+                        <th scope="col" style="width:10%">Note</th>
+                        <th scope="col" style="width:20%">Libellé</th>
+                        <th scope="col" style="width:15%">Date</th>
+                        <th scope="col" style="width:15%">Actions</th>
+                    </tr>
+                        </thead>
                     <?php
-                    }
                     foreach ($commentaires as $commentaire) { ?>
-                        <li class="list-group-item">
-                            <div class="d-flex">
-                                <table>
-                                    <tr>
-                                        <div class="flex-grow-1 align-self-center">
-                                            <td><img src="<?= $commentaire['IMAGE'] ?>" width="170" height="120" /></td>
-                                        </div>
-                                        <div class="flex-grow-1 align-self-center">
-                                            <td><?= $commentaire['LIBELLE'] ?></td>
-                                        </div>
-                                        <div class="flex-grow-1 align-self-center">
-                                            <td><?= $commentaire['NOTECOM'] ?></td>
-                                        </div>
-                                        <div class="flex-grow-1 align-self-center">
-                                            <td><?= $commentaire['LIBELLECOM'] ?></td>
-                                        </div>
-                                        <div class="flex-grow-1 align-self-center">
-                                            <td><?= $commentaire['DATECOM'] ?></td>
-                                        </div>
-                                        <td><a href="./tv?id=<?= $commentaire['IDENTIFIANTVIDEO'] ?>" class="btn btn-outline-secondary">
-                                                <i class="bi bi-pencil-fill"></i></a></td>
-                                        <td><a href="./supprimer?<?= $commentaire['IDCOMMENTAIRE'] ?>" class="btn btn-outline-danger">
-                                            <i class="bi bi-trash"></i></a></td>
-                                    </tr>
-                                </table>
-                            </div>
-                        </li>
-                        <?php
-                    }
-                    if (sizeof($commentaires) == 0) {
+                        <tr>
+                            <td><img src="<?= $commentaire['IMAGE'] ?>" width="185" height="100"/></td>
+                            <td><?= $commentaire['LIBELLE'] ?></td>
+                            <td><?= $commentaire['NOTECOM'] ?></td>
+                            <td><?= $commentaire['LIBELLECOM'] ?></td>
+                            <td><?= $commentaire['DATECOM'] ?></td>
+                            <td><a href="./tv?id=<?= $commentaire['IDENTIFIANTVIDEO'] ?>" class="btn btn-outline-secondary">
+                                    <i class="bi bi-pencil-fill"></i></a>
+                                <a href="./supprimer?<?= $commentaire['IDCOMMENTAIRE'] ?>" class="btn btn-outline-danger">
+                                    <i class="bi bi-trash"></i></a></td>
+                        </tr>
+                    <?php } ?>
+                    </table>
+                <?php } else {
                         ?>
-                        <h3>Vous n'avez pas encore commenté de formations ! Pour découvrir les formations cliquez sur
-                            le
-                            bouton
-                            ci-dessous :</h3>
+                        <h3>Vous n'avez pas encore commenté de formations ! Pour découvrir les formations cliquez sur le bouton ci-dessous :</h3>
                         <a class="w-100 mt-5 btn btn-lg btn-primary" href="./formations">Voir les formations</a>
                         <?php
                     }
