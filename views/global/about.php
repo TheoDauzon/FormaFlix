cddb461aa66789e11caa1431de8ea63868964907
theod <theo.dauzon@gmail.com>
2021-11-25 18:44:29 +0100

    about

--- views/global/about.php
+++ views/global/about.php
@@ -1,11 +1,18 @@
 <div class="container mt-5">
     <div class="row">
         <div class="card">
-            <div class="card-body">
+            <div class="card-body" style="text-align: center;">
                 <h3>FormaFlix</h3>
                 <p>
-                    Formaflix c'est comme Netflix. Mais vous ne regarderez pas des séries mais des formations! Génial !
+                    Formaflix c'est comme Netflix ! Mais vous ne regarderez pas des séries mais des formations.
                 </p>
+                <p>
+                    Vous pourrez même avoir vos certifications ! Vous pouvez y retrouver de nombreux thèmes, comme la sécurité par exemple.
+                </p>
+                <p>
+                    Génial !
+                </p>
+                <a class="w-100 mt-5 btn btn-lg btn-primary" href="./formations">Voir les formations</a>
             </div>
         </div>
     </div>
